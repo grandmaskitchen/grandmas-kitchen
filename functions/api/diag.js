5396ab15053ceba1d74afab2b235a3dba73f183b
grandmaskitchen <garypickett62@gmail.com>
2025-08-12 20:11:06 +0100

    Update diag.js

--- functions/api/diag.js
+++ functions/api/diag.js
@@ -1,1 +1,8 @@
-
+export const onRequestGet = ({ env }) =>
+  new Response(
+    JSON.stringify({
+      hasUrl: !!env.SUPABASE_URL,
+      hasServiceRole: !!env.SUPABASE_SERVICE_ROLE_KEY
+    }),
+    { headers: { 'Content-Type': 'application/json' } }
+  );
