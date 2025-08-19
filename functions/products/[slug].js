6a340d82c8d6392fe2958c7f041395d357aeda87
grandmaskitchen <garypickett62@gmail.com>
2025-08-19 14:19:04 +0100

    Delete functions/products/[slug].js

--- functions/products/[slug].js
+++
@@ -1,6 +0,0 @@
-// Matches /products/<anything> and serves /products/_template.html
-export const onRequestGet = async ({ request, env }) => {
-  const u = new URL(request.url);
-  u.pathname = '/products/_template.html';
-  return env.ASSETS.fetch(new Request(u.toString(), request));
-};
